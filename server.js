9b322f92c4202524cb49e7ffe0d72ee591056da5
Shreyas <shreyashagharkar47@gmail.com>
2026-08-26 10:08:11 +0530

    git push backend

--- server.js
+++ server.js
@@ -1,44 +1,42 @@
 import dotenv from "dotenv";
-import express from "express"
-import { connectDb } from "./db.js"
-import cors from "cors"
-import userRoutes from "./Routes/User.js"
+dotenv.config();
+
+import express from "express";
+import { connectDb } from "./db.js";
+import cors from "cors";
+import userRoutes from "./Routes/User.js";
 import cartRoutes from "./Routes/Cart.js";
 import adminRoutes from "./Routes/Admin.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import productRoutes from "./Routes/product.js";
 import wishlistRoutes from "./Routes/wishlist.js";
 import orderRoutes from "./Routes/order.js";
 
-
-
-dotenv.config(); 
-
 const app = express();
-const port = 3000;
 
 app.use(cors({
-   origin: "https://vercel-frontend-six-liart.vercel.app",
+  origin: "https://vercel-frontend-six-liart.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
-app.use(cookieParser())
-app.use("/youtube/user",userRoutes)
-app.use("/user/cart",cartRoutes)
-app.use("/admin",adminRoutes)
-app.use("/product",productRoutes)
-app.use("/wishlist",wishlistRoutes)
+app.use(cookieParser());
+
+app.use("/youtube/user", userRoutes);
+app.use("/user/cart", cartRoutes);
+app.use("/admin", adminRoutes);
+app.use("/product", productRoutes);
+app.use("/wishlist", wishlistRoutes);
 app.use("/order", orderRoutes);
 
-await connectDb()
+await connectDb();
 
-app.listen(port, (req,res) => {
-  res.status(200).json({
-    message:"server is running"
-  })
-
-  console.log("server running at ", port);
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running"
+  });
 });
+
+export default app;

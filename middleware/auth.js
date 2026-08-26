8157baf43bdb7bb5bc43b45f04880576bb6ee016
Shreyas <shreyashagharkar47@gmail.com>
2026-08-26 18:41:02 +0530

    backend

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,34 +1,39 @@
-import redis from "../config/redis.js";
-
+import redis from "../config/redis";
 export const authMiddleware = async (req, res, next) => {
   try {
 
-    // 1. Browser cookie se token milega
-    const token = req.cookies.authToken;
+   
+
+    
+    const token = await redis.get(`auth:${token}`);
+
+    console.log(
+      "authToken exists:",
+      !!token
+    );
 
     if (!token) {
+
+    
       return res.status(401).json({
         message: "UnAuthorized login,Please Login First",
       });
     }
 
-    // 2. Redis mein token check hoga
-    const session = await redis.get(`auth:${token}`);
+ 
 
-    if (!session) {
-      return res.status(401).json({
-        message: "Invalid or Token Expire",
-      });
-    }
+    const decode = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
 
-    // 3. Redis ke session se user data
-    req.user = JSON.parse(session);
+    req.user = decode;
 
     next();
 
   } catch (error) {
 
-    console.error("Error message:", error.message);
+   
 
     return res.status(401).json({
       message: "Invalid or Token Expire",

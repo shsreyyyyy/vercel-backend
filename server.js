925958509bd2c75be1942b179ea3375a505dc668
Shreyas <shreyashagharkar47@gmail.com>
2026-08-29 16:58:34 +0530

    backend

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/youtube/user", userRoutes);
+app.use("/user", userRoutes);
 app.use("/user/cart", cartRoutes);
 app.use("/admin", adminRoutes);
 app.use("/product", productRoutes);
